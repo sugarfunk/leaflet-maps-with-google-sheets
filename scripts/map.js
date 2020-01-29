5972eb0972593926a7ba7963951d5e254fac9768
sugarfunk <1361962+sugarfunk@users.noreply.github.com>
2020-01-28 22:35:09 -0500

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -935,7 +935,7 @@ $(window).on('load', function() {
     L.Control.Watermark = L.Control.extend({
     onAdd: function(map) {
     var img = L.DomUtil.create('img');
-      img.src = 'https://github.com/sugarfunk/leaflet-maps-with-google-sheets/blob/master/markers/Untitled.png';
+      img.src = 'https://i1.wp.com/f3nation.com/wp-content/uploads/2018/12/cropped-f3_2000x2000_circle.png';
       img.style.width = '200px';
       return img;
     },
