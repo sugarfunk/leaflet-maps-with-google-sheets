5a2cb15aabf9e1ab9460b5ea91c8544dc20b79c9
sugarfunk <1361962+sugarfunk@users.noreply.github.com>
2020-06-10 18:04:25 -0400

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -135,7 +135,7 @@ $(window).on('load', function() {
 
       if (point.Latitude !== '' && point.Longitude !== '') {
         var marker = L.marker([point.Latitude, point.Longitude], {icon: icon})
-          .bindPopup("<center><b>Region:</b>" + point['Region'] + "</center>" + (point['Image'] ? ('<img src="' + point['Image'] + '"><br>') : ('<img src="/markers/Untitled.png"><br>')) +
+          .bindPopup("<center><b>Region:</b>" + point['Region'] + "</center>" + (point['Image'] ? ('<img src="' + point['Image'] + '"><br>') : ('<img src="/scripts/f3.png"><br>')) +
            "<center><h3>" + point['Name'] + '</h3></center><br>' + point['Description']);
 
 //		marker.on('mouseover', function (e) {
