47d593ffefb7d1a67871ff027fc62fb1c812df9a
sugarfunk <1361962+sugarfunk@users.noreply.github.com>
2020-07-13 09:08:50 -0400

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -87,14 +87,15 @@ $(window).on('load', function() {
     var layers = {};
     for (var i in points) {
       var pointLayerNameFromSpreadsheet = points[i].Group;
-      if (layerNamesFromSpreadsheet.indexOf(pointLayerNameFromSpreadsheet) === -1) {
-       markerColors.push(
+      
+	// Sort layer names to match standard week
+        layerNamesFromSpreadsheet.sort(daysOfWeekSorter);
+	if (layerNamesFromSpreadsheet.indexOf(pointLayerNameFromSpreadsheet) === -1) {
+         markerColors.push(
           points[i]['Marker Icon'].indexOf('.') > 0
          ? points[i]['Marker Icon']
           : points[i]['Marker Color']
         );
-	// Sort layer names to match standard week
-        layerNamesFromSpreadsheet.sort(daysOfWeekSorter);
         layerNamesFromSpreadsheet.push(pointLayerNameFromSpreadsheet);
       }
     }
